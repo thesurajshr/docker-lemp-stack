0db3ef13bd97498ef5f7ba24993bf0b1e4e75339
Suraj Shrestha <thesurajshr@gmail.com>
2023-08-03 16:13:01 +1000

    feat(web): Add initial index file

---
+++ www/html/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome to LEMP Stack</title>
+  <style>
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
+
+    body {
+      font-family: 'Helvetica Neue', Arial, sans-serif;
+      background-color: #f9f6f2;
+      color: #333;
+      line-height: 1.6;
+    }
+
+    .container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;  
+    }
+
+    .card {
+      background-color: #fff;
+      border-radius: 6px;
+      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+      padding: 30px;
+      max-width: 600px;
+      width: 80%;
+    }
+
+    h1 {
+      font-size: 36px;
+      margin-bottom: 20px;
+      text-align: center;
+    }
+
+    p {
+      margin-bottom: 20px;
+      font-size: 0.8rem;
+    }
+ 
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="card">
+      <h1>Welcome to LEMP Stack</h1>
+      <p>
+        Thank you for choosing the LEMP stack – a powerful combination of Linux, Nginx, MySQL, and PHP that provides a robust platform for web development and hosting.
+      </p>
+      <p>
+        Whether you're a developer, system administrator, or simply curious about web technologies, you'll find LEMP stack to be a versatile and efficient solution for hosting your web applications.
+      </p>
+      <p>
+        Enjoy exploring the endless possibilities with LEMP stack and building amazing web experiences!
+      </p>
+    </div>
+  </div> 
+</body>
+</html>
